b91ba02feabd26f5c863a8f571d6af852b5a20cf
Cainã Motta <caina.tierling@gmail.com>
2025-11-07 22:51:42 -0300

    Initial Commit

---
+++ FlowOrder/src/main/java/com/api/FlowOrder/FlowOrderApplication.java
@@ -0,0 +1,13 @@
+package com.api.FlowOrder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowOrderApplication.class, args);
+	}
+
+}
---
+++ FlowOrder/src/test/java/com/api/FlowOrder/FlowOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.FlowOrder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
